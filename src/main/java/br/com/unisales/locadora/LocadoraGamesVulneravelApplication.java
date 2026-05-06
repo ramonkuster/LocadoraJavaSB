5a1afa413d72fd3f5b191fd5d7ca3bf050090e3a
Adryan Cordeiro <Adryan.Cordeiro@APEES.ES.GOV.BR.LOCAL>
2026-05-06 10:57:22 -0300

    feat: Setup inicial do projeto de Locadora com Spring Boot

---
+++ src/main/java/br/com/unisales/locadora/LocadoraGamesVulneravelApplication.java
@@ -0,0 +1,13 @@
+package br.com.unisales.locadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoraGamesVulneravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoraGamesVulneravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/unisales/locadora/LocadoraGamesVulneravelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unisales.locadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraGamesVulneravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
